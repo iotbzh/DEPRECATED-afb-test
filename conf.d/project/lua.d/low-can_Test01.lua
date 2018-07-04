c0eabefdcf9dd2f39447e5785a22786e22d09ee3
8000ff <clementmallejac@gmail.com>
2018-07-04 12:13:17 +0200

    Improved test scenarios.
     - Added proper tests.
    
     - Rename test that was using logs to deprecated as they should not be
       used for testing.
    
    Signed-off-by: 8000ff <clementmallejac@gmail.com>

--- conf.d/project/lua.d/low-can_Test01.lua
+++ conf.d/project/lua.d/low-can_Test01.lua
@@ -19,7 +19,8 @@
     NOTE: strict mode: every global variables should be prefixed by '_'
 --]]
 
-_AFT.testCustom("Test_detection_is_off", function()
+-- This tests if a log is printed when a binding subscribes to a signal while the engine is off
+_AFT.testCustom("Test_01/Step_2", function()
     local logMsg = "signal: Engine is off, diagnostic_messages.engine.speed won't received responses until it's on"
     _AFT.addLogToMonitor("low-can", "warning", logMsg)
 
@@ -27,7 +28,8 @@ _AFT.testCustom("Test_detection_is_off", function()
     _AFT.assertLogReceived(logMsg)
 end)
 
-_AFT.testCustom("Test_turning_on", function()
+-- This tests if events are sent to the subscribed bindings
+_AFT.testCustom("Test_01/Step_3", function()
     _AFT.assertVerbStatusSuccess("low-can","subscribe", { event = "diagnostic_messages.engine.speed" })
 
     local evt = "low-can/diagnostic_messages"
@@ -41,10 +43,26 @@ _AFT.testCustom("Test_turning_on", function()
     end)
 end)
 
-_AFT.testCustom("Test_detection_is_on", function()
+-- This asserts that a log message is not sent when a binding subscribes to a signal when the engine is on
+_AFT.testCustom("Test_01/Step_4", function()
     local logMsg = "signal: Engine is off, diagnostic_messages.engine.speed won't received responses until it's on"
     _AFT.addLogToMonitor("low-can", "warning", logMsg)
 
     _AFT.assertVerbStatusSuccess("low-can","subscribe", { event = "diagnostic_messages.engine.speed" })
     _AFT.assertLogNotReceived(logMsg)
+end)
+
+-- This kills the canplayer
+_AFT.testCustom("Test_01/Step_5", function()
+  local ret = os.execute("pkill linuxcan-canpla")
+  _AFT.assertIsTrue(ret)
+end)
+
+-- This checks that the events are sent even after the engine has been turned off after being turned on at least once.
+_AFT.testCustom("Test_01/Step_6", function()
+  local logMsg = "signal: Engine is off, diagnostic_messages.engine.speed won't received responses until it's on"
+  _AFT.addLogToMonitor("low-can", "warning", logMsg)
+
+  _AFT.assertVerbStatusSuccess("low-can","subscribe", { event = "diagnostic_messages.engine.speed" })
+  _AFT.assertLogReceived(logMsg)
 end)
